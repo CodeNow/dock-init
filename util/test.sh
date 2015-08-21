8d38977ed51b319db42e4e8182dc543218946b3f
Ryan Sandor Richards <ryan@runnable.com>
2015-08-21 14:16:36 -0700

    Added test utility script.

---
+++ util/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo FILIBUSTER_VERSION=v0.1.6 \
+     KRAIN_VERSION=v0.0.10 \
+     SAURON_VERSION=v0.0.16 \
+     IMAGE_BUILDER_VERSION=d1.6.2-v3.0.1 \
+     DOCKER_LISTENER_VERSION=v0.8.1 \
+     bash ../init.sh
