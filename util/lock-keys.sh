0128335d41a4432d12d0cc7d956ee488c0227663
Ryan Sandor Richards <ryan@runnable.com>
2015-08-21 14:14:36 -0700

    Fixed lock and unlock keys utility scripts.

--- util/lock-keys.sh
+++ util/lock-keys.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-chmod 600 keys/id_rsa_dock_init
-chmod 600 keys/id_rsa_docker_listener
-chmod 600 keys/id_rsa_filibuster
-chmod 600 keys/id_rsa_hermes_private
-chmod 600 keys/id_rsa_krain
-chmod 600 keys/id_rsa_sauron
+
+KEYS_PATH=/opt/runnable/dock-init/key
+
+chmod 600 $KEYS_PATH/id_rsa_dock_init
+chmod 600 $KEYS_PATH/id_rsa_docker_listener
+chmod 600 $KEYS_PATH/id_rsa_filibuster
+chmod 600 $KEYS_PATH/id_rsa_hermes_private
+chmod 600 $KEYS_PATH/id_rsa_krain
+chmod 600 $KEYS_PATH/id_rsa_sauron
--- util/unlock-keys.sh
+++ util/unlock-keys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-chmod 644 ../keys/id_rsa_dock_init
-chmod 644 ../keys/id_rsa_docker_listener
-chmod 644 ../keys/id_rsa_filibuster
-chmod 644 ../keys/id_rsa_hermes_private
-chmod 644 ../keys/id_rsa_krain
-chmod 644 ../keys/id_rsa_sauron
+
+KEYS_PATH=/opt/runnable/dock-init/key
+chmod 644 $KEYS_PATH/id_rsa_dock_init
+chmod 644 $KEYS_PATH/id_rsa_docker_listener
+chmod 644 $KEYS_PATH/id_rsa_filibuster
+chmod 644 $KEYS_PATH/id_rsa_hermes_private
+chmod 644 $KEYS_PATH/id_rsa_krain
+chmod 644 $KEYS_PATH/id_rsa_sauron
