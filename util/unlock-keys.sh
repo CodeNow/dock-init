12dc276d62bbc7744351a9c61614430b740c35bd
Ryan Sandor Richards <ryan@runnable.com>
2015-08-21 13:46:32 -0700

    Should fix unlock-keys script.

--- util/unlock-keys.sh
+++ util/unlock-keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-chmod 644 keys/id_rsa_dock_init
-chmod 644 keys/id_rsa_docker_listener
-chmod 644 keys/id_rsa_filibuster
-chmod 644 keys/id_rsa_hermes_private
-chmod 644 keys/id_rsa_krain
-chmod 644 keys/id_rsa_sauron
+chmod 644 ../keys/id_rsa_dock_init
+chmod 644 ../keys/id_rsa_docker_listener
+chmod 644 ../keys/id_rsa_filibuster
+chmod 644 ../keys/id_rsa_hermes_private
+chmod 644 ../keys/id_rsa_krain
+chmod 644 ../keys/id_rsa_sauron
