547ce074c9b9a54158c04d4eeb22ce67006abaf0
Ryan Sandor Richards <ryan@runnable.com>
2015-08-21 14:18:18 -0700

    Fixed test script

--- util/test.sh
+++ util/test.sh
@@ -5,4 +5,4 @@ sudo FILIBUSTER_VERSION=v0.1.6 \
      SAURON_VERSION=v0.0.16 \
      IMAGE_BUILDER_VERSION=d1.6.2-v3.0.1 \
      DOCKER_LISTENER_VERSION=v0.8.1 \
-     bash ../init.sh
+     bash /opt/runnable/dock-init/init.sh
