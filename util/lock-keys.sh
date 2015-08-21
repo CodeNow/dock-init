9b377dc319cd208bb1a1b0ac6a60cf80dbedc6d0
Ryan Sandor Richards <ryan@runnable.com>
2015-08-21 13:43:38 -0700

    Added utility scripts for ease of use when building a new AMI.

---
+++ util/lock-keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod 600 keys/id_rsa_dock_init
+chmod 600 keys/id_rsa_docker_listener
+chmod 600 keys/id_rsa_filibuster
+chmod 600 keys/id_rsa_hermes_private
+chmod 600 keys/id_rsa_krain
+chmod 600 keys/id_rsa_sauron
