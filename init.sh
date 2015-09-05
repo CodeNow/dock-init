b37efcad4b58287c7f7fe19ff89fa8a434da02db
Ryan Sandor Richards <ryan@runnable.com>
2015-09-04 18:50:22 -0700

    Concatenate the registry ip on the /etc/hosts file

--- init.sh
+++ init.sh
@@ -22,6 +22,8 @@ RABBITMQ_PASSWORD_PATH=/opt/runnable/rabbitmq_password
 DOCKER_LISTENER_CONF=/etc/init/docker-listener.conf
 SAURON_CONF=/etc/init/sauron.conf
 
+REGISTRY_IP_PATH=/opt/runnable/registry_ip
+
 # Replaces an env directive in the given upstart configuration
 # $1 - Path to the upstart configuration
 # $2 - Environment to replace
@@ -72,6 +74,9 @@ do
   attempt=$(( attempt + 1 ))
   timeout=$(( timeout * 2 ))
 done
+
+# Set correct registry.runnable.com host
+registry_ip=`cat $REGISTRY_IP_PATH " registry.runnable.com"` >> /etc/hosts
 
 # Restart docker
 service docker restart
