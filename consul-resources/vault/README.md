ea93b0536decd118b9aa7f817c24aab437a8d0c4
Bryan Kendall <bryan@runnable.com>
2015-10-30 20:05:46 +0000

    adjust start vault to adjust for multiple environments

--- consul-resources/vault/README.md
+++ consul-resources/vault/README.md
@@ -1,8 +1,15 @@
 # vault credentials
 
-four files need to be created in this folder, each containing one string.
+four files need to be provided to unseal the vault:
 
 - `auth-token`
 - `token-01`
 - `token-02`
 - `token-03`
+
+## environments
+
+each environment has it's own folder for these 4 files:
+
+- `production/*`
+- `production-beta/*`
--- util/start-vault.sh
+++ util/start-vault.sh
@@ -3,6 +3,9 @@ set -e
 
 echo `date` "[INFO] Starting Vault" >> $DOCK_INIT_LOG_PATH
 
+CONSUL_KV_HOST="$CONSUL_HOSTNAME:8500/v1/kv"
+NODE_ENV=$(curl --silent $CONSUL_KV_HOST/node/env | jq --raw-output ".[0].Value" | base64 --decode)
+
 VAULT_CONFIG=$DOCK_INIT_BASE/consul-resources/vault/vault.hcl
 echo `date` "[TRACE] Configuring Vault ($VAULT_CONFIG)" >> $DOCK_INIT_LOG_PATH
 # configure vault
@@ -21,16 +24,17 @@ echo `date` "[TRACE] Authing Vault" >> $DOCK_INIT_LOG_PATH
 # vault unseal and unlock
 VAULT_ADDR="http://$LOCAL_IP4_ADDRESS:8200"
 export VAULT_ADDR
-VAULT_TOKEN=$(cat $DOCK_INIT_BASE/consul-resources/vault/auth-token)
+VAULT_TOKEN=$(cat $DOCK_INIT_BASE/consul-resources/vault/$NODE_ENV/auth-token)
 export VAULT_TOKEN
-rm -f $DOCK_INIT_BASE/consul-resources/vault/auth-token
 echo `date` "[TRACE] Unsealing Vault" >> $DOCK_INIT_LOG_PATH
-vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/token-01` >> $DOCK_INIT_LOG_PATH
-vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/token-02` >> $DOCK_INIT_LOG_PATH
-vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/token-03` >> $DOCK_INIT_LOG_PATH
-rm -f $DOCK_INIT_BASE/consul-resources/vault/token-*
+vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/$NODE_ENV/token-01` >> $DOCK_INIT_LOG_PATH
+vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/$NODE_ENV/token-02` >> $DOCK_INIT_LOG_PATH
+vault unseal `cat $DOCK_INIT_BASE/consul-resources/vault/$NODE_ENV/token-03` >> $DOCK_INIT_LOG_PATH
 
 echo `date` "[TRACE] Vault Status" >> $DOCK_INIT_LOG_PATH
 vault status >> $DOCK_INIT_LOG_PATH
+
+rm -f $DOCK_INIT_BASE/consul-resources/vault/**/auth-token
+rm -f $DOCK_INIT_BASE/consul-resources/vault/**/token-*
 
 echo `date` "[TRACE] Vault Status `vault status`" >> $DOCK_INIT_LOG_PATH
