1c2fb92185919d8ec922dac611d18bd62a392075
Bryan Kendall <bryan@runnable.com>
2015-11-02 15:20:59 -0800

    fix: poll to wait for vault to come up

--- util/start-vault.sh
+++ util/start-vault.sh
@@ -20,10 +20,33 @@ sleep 5
 vault_pid=$!
 echo $vault_pid > /tmp/vault.pid
 
-echo `date` "[TRACE] Authing Vault" >> $DOCK_INIT_LOG_PATH
-# vault unseal and unlock
 VAULT_ADDR="http://$LOCAL_IP4_ADDRESS:8200"
 export VAULT_ADDR
+
+echo `date` "[TRACE] Waiting for Vault to come up" >> $DOCK_INIT_LOG_PATH
+attempt=1
+timeout=1
+while true
+do
+  echo `date` "[INFO] Trying to reach Vault at $VAULT_ADDR $attempt" >> $DOCK_INIT_LOG_PATH
+  if [[ $DOCK_INIT_LOG_STDOUT == 1 ]]
+  then
+    curl -s $VAULT_ADDR/v1/auth/seal-status
+  else
+    curl -s $VAULT_ADDR/v1/auth/seal-status >> $DOCK_INIT_LOG_PATH
+  fi
+
+  if [[ $? == 0 ]]
+  then
+    break
+  fi
+  sleep $timeout
+  attempt=$(( attempt + 1 ))
+  timeout=$(( timeout * 2 ))
+done
+
+echo `date` "[TRACE] Authing Vault" >> $DOCK_INIT_LOG_PATH
+# vault unseal and unlock
 VAULT_TOKEN=$(cat $DOCK_INIT_BASE/consul-resources/vault/$NODE_ENV/auth-token)
 export VAULT_TOKEN
 echo `date` "[TRACE] Unsealing Vault" >> $DOCK_INIT_LOG_PATH
