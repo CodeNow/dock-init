0e17a5b70266eb1ed369b37cd6927bbe4fa72ef3
Ryan Sandor Richards <ryan@runnable.com>
2015-11-19 20:49:12 -0800

    Added run-test script in utils.

---
+++ util/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Helper script for running dock-init by hand during testing
+# @author Ryan Sandor Richards
+
+export CONSUL_HOSTNAME=10.20.1.59
+export DONT_DELETE_KEYS=true
+export LOG_LEVEL=trace
+
+bash /opt/runnable/dock-init/init.sh
