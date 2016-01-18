e353b5c08ff04375e405c24207bcd2a7bcc97144
Casey Flynn <cflynn.us@gmail.com>
2016-01-18 12:06:31 -0800

    Use backoff command with image-builder pull

--- lib/upstart.sh
+++ lib/upstart.sh
@@ -153,10 +153,7 @@ upstart::pull_image_builder() {
     "Dock-Init: Cannot Upstart Services" \
     "Attempting to upstart the services and failing." \
     "${data}"
-  docker pull "registry.runnable.com/runnable/image-builder:$version"
-  if [ $? != 0 ]; then
-    echo "exit here, docker pull returned non-zero"
-  fi
+  backoff docker pull "registry.runnable.com/runnable/image-builder:$version"
   rollbar::clear_trap
 }
 
